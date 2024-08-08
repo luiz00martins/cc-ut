7e64e293f29b5499032443a519bfe7f881072c93
luiz00martins <luizhomartins@gmail.com>
2024-08-07 22:49:59 -0300

    feat: add instances Includes 'verbose' option.

--- init.lua
+++ init.lua
@@ -2,220 +2,236 @@ local utils = require('cc-ut.utils')
 
 local table_compare_by_value = utils.table_compare_by_value
 
--- Utility to compare values and print results with colors
-local function printResult(testName, failed, indent)
-  indent = indent or ""
-  if #failed == 0 then
-    term.setTextColor(colors.green)
-    print(indent .. '+ ' .. testName)
-  else
-    for _, errorMessage in ipairs(failed) do
-      if errorMessage == 'No expectations set' then
-        term.setTextColor(colors.yellow)
-        print(indent .. '* ' .. testName .. ': ' .. errorMessage)
+local function instance(config)
+  config = config or {}
+  local verbose = config.verbose ~= false  -- Default to true if not specified
+
+  -- Utility to compare values and print results with colors
+  local function printResult(testName, failed, indent)
+    if not verbose then return end
+    indent = indent or ""
+    if #failed == 0 then
+      term.setTextColor(colors.green)
+      print(indent .. '+ ' .. testName)
+    else
+      for _, errorMessage in ipairs(failed) do
+        if errorMessage == 'No expectations set' then
+          term.setTextColor(colors.yellow)
+          print(indent .. '* ' .. testName .. ': ' .. errorMessage)
+        else
+          term.setTextColor(colors.red)
+          print(indent .. '- ' .. testName .. ': ' .. errorMessage)
+        end
+      end
+    end
+    term.setTextColor(colors.white)
+  end
+
+  local function createExpectation(evaluated, passed, failed)
+    local expectation = {
+      evaluated = evaluated,
+      passed = passed or {},
+      failed = failed or {}
+    }
+
+    local expect = function(value)
+      local self = {}
+
+      function self.toBe(expected)
+        expectation.evaluated = true
+        if value ~= expected then
+          table.insert(expectation.failed, 'Expected ' .. tostring(value) .. ' to be ' .. tostring(expected))
+          return self
+        end
+        table.insert(expectation.passed, 'Expected ' .. tostring(value) .. ' to be ' .. tostring(expected))
+        return self
+      end
+
+      function self.toEqual(expected)
+        expectation.evaluated = true
+        if not table_compare_by_value(value, expected) then
+          table.insert(expectation.failed, 'Expected ' .. tostring(value) .. ' to equal ' .. tostring(expected))
+          return self
+        end
+        table.insert(expectation.passed, 'Expected ' .. tostring(value) .. ' to equal ' .. tostring(expected))
+        return self
+      end
+
+      function self.toThrow()
+        expectation.evaluated = true
+        local success, result = pcall(value)
+        if success then
+          table.insert(expectation.failed, 'Expected function to throw an error')
+          return self
+        end
+        table.insert(expectation.passed, 'Expected function to throw an error')
+        return self
+      end
+
+      function self.toContain(expected)
+        expectation.evaluated = true
+        if not utils.table_contains(value, expected) then
+          table.insert(expectation.failed, 'Expected ' .. tostring(value) .. ' to contain ' .. tostring(expected))
+          return self
+        end
+        table.insert(expectation.passed, 'Expected ' .. tostring(value) .. ' to contain ' .. tostring(expected))
+        return self
+      end
+
+      function self.toBeTruthy()
+        expectation.evaluated = true
+        if not value then
+          table.insert(expectation.failed, 'Expected ' .. tostring(value) .. ' to be truthy')
+          return self
+        end
+        table.insert(expectation.passed, 'Expected ' .. tostring(value) .. ' to be truthy')
+        return self
+      end
+
+      return self
+    end
+
+    expectation.expect = expect
+
+    return expectation
+  end
+
+  local function test(testName, func)
+    if not func then
+      error('No function provided')
+    end
+
+    local expectation = createExpectation(false, nil, nil)
+
+    local success, err = pcall(func, expectation.expect)
+
+    if not success then
+      printResult(testName, {err})
+      return {
+        name = testName,
+        status = 'failed',
+        failed = {err},
+      }
+    elseif not expectation.evaluated then
+      local error_message = 'No expectations set'
+      printResult(testName, {error_message})
+      return {
+        name = testName,
+        status = 'failed',
+        failed = {error_message},
+      }
+    else
+      printResult(testName, expectation.failed)
+
+      if #expectation.failed > 0 then
+        return {
+          name = testName,
+          status = 'failed',
+          failed = expectation.failed,
+        }
       else
-        term.setTextColor(colors.red)
-        print(indent .. '- ' .. testName .. ': ' .. errorMessage)
+        return {
+          name = testName,
+          status = 'passed',
+          passed = expectation.passed,
+        }
       end
     end
   end
-  term.setTextColor(colors.white)
+
+  local function describe(description, block)
+    if not block then
+      error('No block provided')
+    end
+
+    local tests = {}
+    local failed = {}
+    local passed = {}
+
+    local function localTest(testName, func)
+      table.insert(tests, {name = testName, func = func})
+    end
+
+    block(localTest)
+
+    if verbose then
+      -- Initial output
+      print(description)
+      term.write('Running tests: [' .. string.rep(' ', 10) .. '] (0/' .. #tests .. ')')
+    end
+
+    for i, test in ipairs(tests) do
+      local expectation = createExpectation(false, nil, nil)
+      local success, err = pcall(test.func, expectation.expect)
+      if not success then
+        expectation.failed = {err}
+      elseif not expectation.evaluated then
+        expectation.failed = {'No expectations set'}
+        success = false
+      else
+        success = #expectation.failed == 0
+      end
+
+      if not success then
+        table.insert(failed, {
+          name = test.name,
+          failed = expectation.failed,
+        })
+      else
+        table.insert(passed, {
+          name = test.name,
+          passed = expectation.passed,
+        })
+      end
+
+      if verbose then
+        -- Update progress bar
+        term.setCursorPos(1, select(2, term.getCursorPos()) - 1)
+        term.clearLine()
+        term.write('. ' .. description)
+        term.setCursorPos(1, select(2, term.getCursorPos()) + 1)
+        term.clearLine()
+        local progress = math.floor((i / #tests) * 10)
+        term.write('Running tests: [' .. string.rep('#', progress) .. string.rep(' ', 10 - progress) .. '] (' .. i .. '/' .. #tests .. ')')
+      end
+    end
+
+    if verbose then
+      -- Clear lines for final message
+      term.setCursorPos(1, select(2, term.getCursorPos()) - 1)
+      term.clearLine()
+      term.setCursorPos(1, select(2, term.getCursorPos()) + 1)
+      term.clearLine()
+      term.setCursorPos(1, select(2, term.getCursorPos()) - 1)
+
+      -- Final message
+      if #failed == 0 then
+        term.setTextColor(colors.green)
+        print('+ ' .. description .. ' (' .. #passed .. '/' .. #tests .. ')')
+      else
+        term.clearLine()
+        term.setTextColor(colors.red)
+        print('- ' .. description .. ' (' .. #passed .. '/' .. #tests .. ')')
+        for _, test in ipairs(failed) do
+          printResult(test.name, test.failed, '  ')
+        end
+      end
+
+      term.setTextColor(colors.white)
+    end
+
+    return {
+      name = description,
+      status = #failed > 0 and 'failed' or 'passed',
+      failed = failed,
+      passed = passed,
+    }
+  end
+
+  return {
+    test = test,
+    describe = describe
+  }
 end
 
-local function createExpectation(evaluated, passed, failed)
-	local expectation = {
-		evaluated = evaluated,
-		passed = passed or {},
-		failed = failed or {}
-	}
-
-	local expect = function(value)
-		local self = {}
-
-		function self.toBe(expected)
-			expectation.evaluated = true
-			if value ~= expected then
-				table.insert(expectation.failed, 'Expected ' .. tostring(value) .. ' to be ' .. tostring(expected))
-				return self
-			end
-			table.insert(expectation.passed, 'Expected ' .. tostring(value) .. ' to be ' .. tostring(expected))
-			return self
-		end
-
-		function self.toEqual(expected)
-			expectation.evaluated = true
-			if not table_compare_by_value(value, expected) then
-				table.insert(expectation.failed, 'Expected ' .. tostring(value) .. ' to equal ' .. tostring(expected))
-				return self
-			end
-			table.insert(expectation.passed, 'Expected ' .. tostring(value) .. ' to equal ' .. tostring(expected))
-			return self
-		end
-
-		function self.toThrow()
-			expectation.evaluated = true
-			local success, result = pcall(value)
-			if success then
-				table.insert(expectation.failed, 'Expected function to throw an error')
-				return self
-			end
-			table.insert(expectation.passed, 'Expected function to throw an error')
-			return self
-		end
-
-		function self.toContain(expected)
-			expectation.evaluated = true
-			if not utils.table_contains(value, expected) then
-				table.insert(expectation.failed, 'Expected ' .. tostring(value) .. ' to contain ' .. tostring(expected))
-				return self
-			end
-			table.insert(expectation.passed, 'Expected ' .. tostring(value) .. ' to contain ' .. tostring(expected))
-			return self
-		end
-
-		function self.toBeTruthy()
-			expectation.evaluated = true
-			if not value then
-				table.insert(expectation.failed, 'Expected ' .. tostring(value) .. ' to be truthy')
-				return self
-			end
-			table.insert(expectation.passed, 'Expected ' .. tostring(value) .. ' to be truthy')
-			return self
-		end
-
-		return self
-	end
-
-	expectation.expect = expect
-
-	return expectation
-end
-
--- Test function that runs a test case
-local function test(testName, func)
-	if not func then
-		error('No function provided')
-	end
-
-	local expectation = createExpectation(false, nil, nil)
-
-	local success, err = pcall(func, expectation.expect)
-
-	if not success then
-		printResult(testName, {err})
-		return {
-			status = 'failed',
-			failed = {err},
-		}
-	elseif not expectation.evaluated then
-		local error_message = 'No expectations set'
-		printResult(testName, {error_message})
-		return {
-			status = 'failed',
-			failed = {error_message},
-		}
-	else
-		printResult(testName, expectation.failed)
-
-		if #expectation.failed > 0 then
-			return {
-				status = 'failed',
-				failed = expectation.failed,
-			}
-		else
-			return {
-				status = 'passed',
-				passed = expectation.passed,
-			}
-		end
-	end
-end
-
--- Describe block to group and execute tests
-local function describe(description, block)
-	if not block then
-		error('No block provided')
-	end
-
-	local tests = {}
-	local failed = {}
-	local passed = {}
-
-	local function localTest(testName, func)
-		table.insert(tests, {name = testName, func = func})
-	end
-
-	block(localTest)
-
-	-- Initial output
-	print(description)
-	term.write('Running tests: [' .. string.rep(' ', 10) .. '] (0/' .. #tests .. ')')
-
-	for i, test in ipairs(tests) do
-		local expectation = createExpectation(false, nil, nil)
-		local success, err = pcall(test.func, expectation.expect)
-		if not success then
-			expectation.failed = {err}
-		elseif not expectation.evaluated then
-			expectation.failed = {'No expectations set'}
-			success = false
-		else
-			success = #expectation.failed == 0
-		end
-
-		if not success then
-			table.insert(failed, {
-				name = test.name,
-				failed = expectation.failed,
-			})
-		else
-			table.insert(passed, {
-				name = test.name,
-				passed = expectation.passed,
-			})
-		end
-
-		-- Update progress bar
-		term.setCursorPos(1, select(2, term.getCursorPos()) - 1)
-		term.clearLine()
-		term.write('. ' .. description)
-		term.setCursorPos(1, select(2, term.getCursorPos()) + 1)
-		term.clearLine()
-		local progress = math.floor((i / #tests) * 10)
-		term.write('Running tests: [' .. string.rep('#', progress) .. string.rep(' ', 10 - progress) .. '] (' .. i .. '/' .. #tests .. ')')
-	end
-
-	-- Clear lines for final message
-	term.setCursorPos(1, select(2, term.getCursorPos()) - 1)
-	term.clearLine()
-	term.setCursorPos(1, select(2, term.getCursorPos()) + 1)
-	term.clearLine()
-	term.setCursorPos(1, select(2, term.getCursorPos()) - 1)
-
-	-- Final message
-	if #failed == 0 then
-		term.setTextColor(colors.green)
-		print('+ ' .. description .. ' (' .. #passed .. '/' .. #tests .. ')')
-	else
-		term.clearLine()
-		term.setTextColor(colors.red)
-		print('- ' .. description .. ' (' .. #passed .. '/' .. #tests .. ')')
-		for _, test in ipairs(failed) do
-			printResult(test.name, test.failed, '  ')
-		end
-	end
-
-	term.setTextColor(colors.white)
-
-	return {
-		status = #failed > 0 and 'failed' or 'passed',
-		failed = failed,
-		passed = passed,
-	}
-end
-
-return {
-	test = test,
-	describe = describe
-}
-
+return instance
--- test.lua
+++ test.lua
@@ -1,70 +1,92 @@
-local ut = require("cc-ut")
+local Ut = require("cc-ut")
+
+local ut = Ut({ verbose = false })
 
 local test = ut.test
 local describe = ut.describe
 
 local function assert_equals(value, expected)
-	assert(value == expected, 'Expected ' .. textutils.serialize(value) .. ' to be ' .. textutils.serialize(expected))
+  assert(value == expected, 'Expected ' .. textutils.serialize(value) .. ' to be ' .. textutils.serialize(expected))
+end
+
+local function printTestResult(result)
+  if result.status == 'passed' then
+    print('+ ' .. result.name .. ' (' .. #result.passed .. ' passed)')
+  else
+    print('- ' .. result.name .. ' (' .. #result.failed .. ' failed)')
+    for _, failures in ipairs(result.failed) do
+      for _, failureMessage in ipairs(failures) do
+        print('  - ' .. failureMessage)
+      end
+    end
+  end
 end
 
 local testResult = nil
 
 testResult = test("test passing", function(expect)
-	expect(1).toBe(1)
+  expect(1).toBe(1)
 end)
+printTestResult(testResult)
 assert_equals(testResult.status, 'passed')
 assert_equals(testResult.passed[1], 'Expected 1 to be 1')
 
 testResult = test("test correctly throwing", function(expect)
-	expect(function() error("test error") end).toThrow()
+  expect(function() error("test error") end).toThrow()
 end)
+printTestResult(testResult)
 assert_equals(testResult.status, 'passed')
 assert_equals(testResult.passed[1], 'Expected function to throw an error')
 
 testResult = test("test failing", function(expect)
-	expect(1).toBe(0)
+  expect(1).toBe(0)
 end)
+printTestResult(testResult)
 assert_equals(testResult.status, 'failed')
 assert_equals(testResult.failed[1], 'Expected 1 to be 0')
 
 testResult = test("no test", function(expect)
-	return
+  return
 end)
+printTestResult(testResult)
 assert_equals(testResult.status, 'failed')
 assert_equals(testResult.failed[1], 'No expectations set')
 
 testResult = test("test wrongfully throwing", function(expect)
-	error("test error")
+  error("test error")
 end)
+printTestResult(testResult)
 assert_equals(testResult.status, 'failed')
-assert_equals(testResult.failed[1], 'test.lua:49: test error')
+assert_equals(testResult.failed[1], '/cc-ut/test.lua:56: test error')
 
 testResult = describe('successful describe', function(test)
-	test("matching 1", function(expect)
-		expect(1).toBe(1)
-	end)
+  test("matching 1", function(expect)
+    expect(1).toBe(1)
+  end)
 
-	test("matching 2", function(expect)
-		expect(1).toBe(1)
-	end)
+  test("matching 2", function(expect)
+    expect(1).toBe(1)
+  end)
 
-	test("matching 3", function(expect)
-		expect(1).toBe(1)
-	end)
+  test("matching 3", function(expect)
+    expect(1).toBe(1)
+  end)
 end)
+printTestResult(testResult)
 assert_equals(testResult.status, 'passed')
 assert_equals(testResult.passed[1].passed[1], 'Expected 1 to be 1')
 assert_equals(testResult.passed[2].passed[1], 'Expected 1 to be 1')
 assert_equals(testResult.passed[3].passed[1], 'Expected 1 to be 1')
 
 testResult = describe('failed describe', function(test)
-	test("wrong value", function(expect)
-		expect(1).toBe(0)
-	end)
+  test("wrong value", function(expect)
+    expect(1).toBe(0)
+  end)
 
-	test("no expectation", function(expect)
-	end)
+  test("no expectation", function(expect)
+  end)
 end)
+printTestResult(testResult)
 assert_equals(testResult.status, 'failed')
 assert_equals(testResult.failed[1].failed[1], 'Expected 1 to be 0')
 assert_equals(testResult.failed[2].failed[1], 'No expectations set')
